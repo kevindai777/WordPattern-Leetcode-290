f3418065892843b088b9c55f5b87bf77989458c0
kevindai777 <kevin.dai.2451@gmail.com>
2020-08-03 10:56:46 -0400

    solutions

---
+++ wordpattern.js
@@ -0,0 +1,25 @@
+//Objective is to see if a string follows a given pattern
+
+let pattern = "abba", str = "dog cat cat dog"
+
+
+//O(n) solution that uses a hashmap to keep track of patterns to words
+
+let map = {}
+let arr = str.split(' ')
+
+if (pattern.length != arr.length) {
+    return false
+}
+
+for (let i = 0; i < pattern.length; i++) {
+    if (!map[pattern[i]] && !Object.values(map).includes(arr[i])) {
+        map[pattern[i]] = arr[i]
+    } else {
+        if (map[pattern[i]] != arr[i]) {
+            return false
+        }
+    }
+}
+
+return true
